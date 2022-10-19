b1bb53f0a64ecf63cd3c50293fddf4afcbf5e6ce
ooyuo <doyounjeong0@gmail.com>
2022-10-19 17:21:49 -0400

    handling form submission

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,23 @@
-import './ExpenseItem.css';
-import ExpenseDate from './ExpenseDate';
-import ExpensePrice from './ExpenseAmount';
-import Card from '../UI/Card';
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import ExpensePrice from "./ExpenseAmount";
+import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-
-    return (
-        <Card className='expense-item'>
-            <ExpenseDate date={props.date}/>
-            <div className='expense-item__description'>
-                <h2>{props.title}</h2>
-                <ExpensePrice amount={props.amount} />
-            </div>
-        </Card>    
-    )
-}
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = () => {
+    setTitle("Updated!");
+  };
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Change Title</button>
+      </div>
+    </Card>
+  );
+};
 
 export default ExpenseItem;
