c0a370c65aa6fd5b05bfdd5e887ef04ed53301e7
ooyuo <doyounjeong0@gmail.com>
2022-10-29 20:20:35 -0400

    렌더링 리스트

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -5,6 +5,7 @@ import Card from "../UI/Card";
 import { useState } from "react";
 
 const ExpenseItem = (props) => {
+  console.log(props);
   const [title, setTitle] = useState(props.title);
   const clickHandler = () => {
     setTitle("Updated!");
@@ -13,7 +14,7 @@ const ExpenseItem = (props) => {
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
         <button onClick={clickHandler}>Change Title</button>
       </div>
